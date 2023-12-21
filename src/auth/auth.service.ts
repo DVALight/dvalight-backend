71905cfe79516f819abcf31a1038f71d5d32b5ae
Na3ar-17 <gavruk2017@gmail.com>
2023-12-21 14:13:33 +0200

    installed nest-jwt, and configured jwt-service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { LoginDto } from './dto/auth.dto';
 import { UserService } from 'src/user/user.service';
 import { compare } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
   async login(dto: LoginDto) {
     const user = await this.validateUser(dto);
   }
