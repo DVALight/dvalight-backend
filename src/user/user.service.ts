ef03e7eefece7d42ba9fc682e8c301191b8a19c4
Na3ar-17 <gavruk2017@gmail.com>
2023-12-21 18:22:00 +0200

    fixed bug with registration

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -20,7 +20,8 @@ export class UserService {
 
     const newUser = await this.prisma.users.create({
       data: {
-        ...dto,
+        name: dto.name,
+        email: dto.email,
         password: await hash(dto.password, 10),
       },
     });
